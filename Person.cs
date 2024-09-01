128485f04e85cae5437acaf27a2d968d72eea0e7
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-09-01 10:49:24 -0400

    structs

---
+++ Person.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace classes_01;
+
+struct Person
+{
+    public string name;
+    public int age;
+
+    // constructor
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using classes_01;
+
+// STRUCTS
+
+// 1. basic concept
+
+// Person person01;
+// person01.name = "John";
+// person01.age = 23; 
+
+// 2. using structs in a function
+
+// static Person ReturnPerson()
+// {
+//     Person person;
+//     System.Console.WriteLine("Enter your name.");
+//     person.name = Console.ReadLine() ?? "";
+    
+//     System.Console.WriteLine("Enter your age.");
+//     person.age = Convert.ToInt32(Console.ReadLine());
+
+//     return person;
+// }
+
+// Person person02 = ReturnPerson();
+
+// System.Console.WriteLine($"{person02.name} - {person02.age}");
+
+// 3. constructors
+
+static Person ReturnPerson()
+{
+    System.Console.WriteLine("Enter your name.");
+    string name = Console.ReadLine() ?? "";
+    
+    System.Console.WriteLine("Enter your age.");
+    int age = Convert.ToInt32(Console.ReadLine());
+
+    return new Person(name, age);
+}
+
+Person person02 = ReturnPerson();
+
+System.Console.WriteLine($"{person02.name} - {person02.age}");
